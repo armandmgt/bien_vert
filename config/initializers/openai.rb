e7682c79b4532b8692794564a94c15312f677680
Armand Mégrot <armand.megrot@gmail.com>
2025-04-05 19:29:22 +0200

    Avoid crash on credentials during assets precompile

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openai["api_key"]
+  config.access_token = Rails.application.credentials.dig(:openai, :api_key)
   config.log_errors = Rails.env.local?
 end
