da47ecd683a110d53b519121b5436e4bfa521992
Armand Mégrot <armand.megrot@gmail.com>
2025-06-29 16:23:16 +0200

    Fix db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ Rpush::Webpush::App.find_or_create_by!(name: "webpush") do |app|
 end
 
 Rpush::Apnsp8::App.find_or_create_by!(name: "apnsp8") do |app|
-  app.environment = Rails.env.local? ? :sandbox : :production
+  app.environment = Rails.env.local? ? :development : :production
   app.apn_key_id = Rails.application.credentials.dig(:apn, :key_id)
   app.apn_key = Base64.decode64(Rails.application.credentials.dig(:apn, :key))
   app.team_id = Rails.application.credentials.dig(:apn, :team_id)
